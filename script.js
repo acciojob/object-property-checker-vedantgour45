3706051f992275112e6313310092e58d2a7fa8af
Vedant Gour <113295244+vedantgour45@users.noreply.github.com>
2023-04-11 08:50:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,13 @@
-const sampleObject = { red: "#FF0000", green: "#00FF00", white: "#FFFFFF" };
+const sampleObject = { 
+	red: "#FF0000", 
+	green: "#00FF00", 
+	white: "#FFFFFF" 
+};
 
 function hasKey(key) {
   //   write your code here
+		
+	return sampleObject.hasOwnProperty(key)); 
 }
 
 // Do not change the code below
